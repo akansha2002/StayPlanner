2f800a96fa7fcf0b7abbcb03ef27ec4181482fd5
Akansha Aggarwal <akanshaaggarwal9876@gmail.com>
2023-07-23 16:42:28 +0530

    Basic Routes added

--- api/index.js
+++ api/index.js
@@ -1,7 +1,40 @@
 import express from "express"
-const app = express()
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import authRoute from "./routes/auth"
+import usersRoute from "./routes/users"
+import hotelsRoute from "./routes/hotels"
+import roomsRoute from "./routes/rooms"
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB.")
+    } catch (error) {
+        throw(error)
+    }
+}
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mmongoDB disconnected");
+})
+
+mongoose.connection.on("connected", () => {
+    console.log("mmongoDB connected");
+})
+
+
+// middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 app.listen(8800, () => {
+    connect();
     console.log("Connected to backend.")
 })
 
